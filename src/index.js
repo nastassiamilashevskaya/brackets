5dc4586ea07096739ac10df7c999c97b31b786cc
nastassiamilashevskaya <52507944+nastassiamilashevskaya@users.noreply.github.com>
2020-03-08 18:35:22 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let i = 0;
+  while (i < bracketsConfig.length) {
+    let brackets = bracketsConfig[i].join('');
+
+    if (str.includes(brackets)) {
+      str = str.replace(brackets, '');
+      i = 0;
+    } else {
+      i++;
+    }
+  }
+
+  if (str.length === 0) {
+    return true;
+  } else {
+    return false;
+  }
 }
